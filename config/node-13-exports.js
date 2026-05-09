3f514f75cc2056ae7a05645bc366b15299a72029
Jordan Ramsay <jordanr123@bigpond.com>
2026-05-09 13:54:55 +1000

    Path OS Agnostic Run

--- config/node-13-exports.js
+++ config/node-13-exports.js
@@ -1,10 +1,11 @@
 const fs = require('fs');
+const path = require('path');
 
 const copy = (filename) => {
 	// Copy .module.js --> .mjs for Node 13 compat.
 	fs.writeFileSync(
-		`${process.cwd()}/dist/${filename}.mjs`,
-		fs.readFileSync(`${process.cwd()}/dist/${filename}.module.js`)
+		path.join(process.cwd(), 'dist', `${filename}.mjs`),
+		fs.readFileSync(path.join(process.cwd(), 'dist', `${filename}.module.js`))
 	);
 };
 
--- config/node-verify-exports.js
+++ config/node-verify-exports.js
@@ -1,7 +1,9 @@
 const path = require('path');
 const assert = require('assert/strict');
+const { pathToFileURL } = require('url');
 
 const filePath = (file) => path.join(process.cwd(), 'dist', file);
+const fileURL = (file) => pathToFileURL(filePath(file)).href;
 
 // Main CJS
 const mainCjs = require(filePath('index.js'));
@@ -12,7 +14,7 @@ assert(typeof mainCjs.render === 'function');
 
 // Main ESM
 (async () => {
-	const mainESM = await import(filePath('index.mjs'));
+	const mainESM = await import(fileURL('index.mjs'));
 	assert(typeof mainESM.default === 'function');
 	assert(typeof mainESM.renderToString === 'function');
 	assert(typeof mainESM.renderToStaticMarkup === 'function');
@@ -27,7 +29,7 @@ assert(typeof jsxCjs.shallowRender === 'function');
 
 // JSX ESM
 (async () => {
-	const jsxESM = await import(filePath('jsx/index.mjs'));
+	const jsxESM = await import(fileURL('jsx/index.mjs'));
 	assert(typeof jsxESM.default === 'function');
 	assert(typeof jsxESM.render === 'function');
 	assert(typeof jsxESM.shallowRender === 'function');
